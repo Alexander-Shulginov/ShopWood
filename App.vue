831a6a4f099a48818679bc13e960b0a8fe9491f7
J_lx010-7 <alexander3post@gmail.com>
2023-08-26 16:49:57 +0300

    add link

--- App.vue
+++ App.vue
@@ -1,5 +1,11 @@
 <template>
 	<div class="container">
+		<div class="link">
+			Основано на ТЗ -
+			<a
+				href="https://docs.google.com/document/d/1_BN8bbym0InYFDElyyVJ5GzPptaOe0p9kteX-Uaww3I/edit#heading=h.ikkq5tsgq96y"
+			></a>
+		</div>
 		<NavBar />
 		<SearchSection />
 		<router-view></router-view>
@@ -21,5 +27,12 @@
 		max-width: 1200px;
 		padding: 0 15px;
 		margin: 0 auto;
+	}
+
+	.link {
+		font-family: sans-serif;
+		text-align: center;
+		padding: 6px 0;
+		background-color: rgb(25, 189, 214);
 	}
 </style>
